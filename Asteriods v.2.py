ba62d5518f05a70ffadd63e26f850fb60371705a
Ivital7 <31019482+Ivital7@users.noreply.github.com>
2017-08-14 19:53:25 -0400

    Add Colours
    Added Yellow and Blue to Colour Options

--- Asteriods v.2.py
+++ Asteriods v.2.py
@@ -10,6 +10,8 @@ BLACK  = (  0,   0,   0)
 RED    = (255,   0,   0)
 GREEN  = (  0, 255,   0)
 ORANGE = (255, 128,   0)
+YELLOW = (255, 255,   0)
+BLUE   = (0  ,   0, 255)  
 
 list_bullets=[]
 list_asteroids=[]
